dc01594e78ba4643608aebd38460a62ff6397f01
MichaelHaes <115067526+MichaelHaes@users.noreply.github.com>
2023-08-21 14:14:34 +0700

    Update dashboard.blade.php

--- App-HRIS/resources/views/dashboard.blade.php
+++ App-HRIS/resources/views/dashboard.blade.php
@@ -1,7 +1,12 @@
 @extends('layout.app')
 
 @section('content')
-
-<h1>Admin Dashboard!</h1>
-
+<br>
+<div class="d-flex justify-content-between">
+    <div class="card">
+        <div class="card-body">
+            This is some text within a card body.
+        </div>
+    </div>
+</div>
 @stop
