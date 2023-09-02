4b2e2b76fbb28d8d968461fde882921cfd6ad2a1
ardhitosen <78494962+ardhitosen@users.noreply.github.com>
2023-09-02 13:49:23 +0700

    cobain calendar baru

--- App-HRIS/resources/views/timeManagement/calendar.blade.php
+++ App-HRIS/resources/views/timeManagement/calendar.blade.php
@@ -1,14 +1,24 @@
 @extends('layout.app')
 
 @section('content')
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
+
+</script>
 <br>
 <div class="card">
     <div class="card-body">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-10">
-                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FJakarta&src=Y183NTU3NmIyZjFmZDEyYTZkODFmNWViYmVlMTc4YzMwMmI1ZjhiNDc0MjVmNzQwOTU5ODQ5Mzk1ODM5ZGFmOWU4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uaW5kb25lc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F4511E&color=%230B8043" style="border:none" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-                </div>
+                <div id='calendar'></div>
             </div>
         </div>
     </div>
