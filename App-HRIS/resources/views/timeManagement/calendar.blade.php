67b3cf328c744325b5c79f6a5e909c6c4ee6eefe
MichaelHaes <115067526+MichaelHaes@users.noreply.github.com>
2023-09-05 20:16:38 +0700

    Update calendar.blade.php

--- App-HRIS/resources/views/timeManagement/calendar.blade.php
+++ App-HRIS/resources/views/timeManagement/calendar.blade.php
@@ -3,24 +3,29 @@
 @section('content')
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 <script>
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
+      initialView: 'dayGridMonth',
+      buttonText: {
+        today: 'Today',
+        month: 'Month',
+        week: 'Week',
+        day: 'Day',
+        list: 'List'
+      }
     });
     calendar.render();
   });
-
 </script>
 <br>
 <div class="card">
-    <div class="card-body">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div id='calendar'></div>
-            </div>
-        </div>
+  <div class="card-body">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div id='calendar'></div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
