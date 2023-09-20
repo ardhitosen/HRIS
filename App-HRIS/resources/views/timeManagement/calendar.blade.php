26fca9e07d1a0fe9c24d298753912d5c0167873b
MichaelHaes <115067526+MichaelHaes@users.noreply.github.com>
2023-09-20 09:34:07 +0700

    calendar wip

--- App-HRIS/resources/views/timeManagement/calendar.blade.php
+++ App-HRIS/resources/views/timeManagement/calendar.blade.php
@@ -4,10 +4,32 @@
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 <script src='fullcalendar/google-calendar/main.js'></script>
 <script>
+    import {
+        Calendar
+    } from '@fullcalendar/core'; // Import the core Calendar class
+    import interactionPlugin from '@fullcalendar/interaction'; // Import the interaction plugin
+
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
+            height: 600,
             initialView: 'dayGridMonth',
+            plugins: [interactionPlugin],
+            editable: true,
+            droppable: true,
+            customButtons: {
+                addEvent: {
+                    text: 'Add Event',
+                    click: function() {
+                        alert('clicked the custom button!');
+                    }
+                }
+            },
+            headerToolbar: {
+                start: 'title',
+                center: 'addEvent',
+                end: 'today prev,next'
+            },
             buttonText: {
                 today: 'Today',
                 month: 'Month',
@@ -15,19 +37,16 @@
                 day: 'Day',
                 list: 'List'
             },
-            // plugins: [ 'googleCalendar' ],
-            // googleCalendarApiKey: 'AIzaSyD31VE3KMcRKOjddUdwhywNBLbJMOHB_88',
-            // events: {
-            //   googleCalendarId: 'cddf74f07479800feaaa393add109f00e896c722f5bcf9b0c2c5653d1cf02c96@group.calendar.google.com'
-            // }, ga paham
-            eventClick: {
-
+            events: [{
+                title: 'Test Events',
+                start: '2023-09-01',
+                end: '2023-09-02'
+            }],
+            eventClick: function(info) {
+                alert('Event: ' + info.event.title);
             },
-            headerToolbar: {
-                start: 'title', // will normally be on the left. if RTL, will be on the right
-                center: '',
-                end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
-            }
+            droppable: true,
+            selectable: true
         });
         calendar.render();
     });
@@ -37,7 +56,8 @@
     <div class="card-body">
         <div class="container">
             <div class="row justify-content-center">
-                <div id='calendar'></div>
+                <div id='calendar'>
+                </div>
             </div>
         </div>
     </div>
