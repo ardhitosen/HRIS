462be7cc440e21162334b832bdd32f9b313e05d7
Michael Harry Setiawan <michael.mhs70@gmail.com>
2023-09-26 20:01:48 +0700

    profile data
    tinggal di design aja + mungking ada yang ditambahin lagi belom tau

--- App-HRIS/resources/views/frontend/profile.blade.php
+++ App-HRIS/resources/views/frontend/profile.blade.php
@@ -26,10 +26,25 @@
     <h3 class=>Profile</h3>
     <div class="card-body">
         <div class="card">
-            <div class="card-header">
-            </div>
-            <div class="card-body">
-                
+            <div class="card-body" id="profile_template">
+                <div class="d-flex flex-column align-items-center">
+                    <img src="data:image/jpeg;base64,{{ base64_encode(session('employee')->photo) }}" alt="" width="50%">
+                    <h4>{{ session('employee')->job_level }} {{ session('employee')->job_position }}</h4>
+                    <h4>{{ session('employee')->organization }}</h4>
+                </div>
+                <div>
+                    <h2>Employee Information</h2>
+                    <hr>
+                    <p>Name: {{ session('employee')->name}}</p>
+                    <p>Address: {{ session('employee')->address}}</p>
+                    <p>Phone Number: {{ session('employee')->mobile_phone}}</p>
+                    <h2>Employment Status</h2>
+                    <hr>
+                    <p>Employement: {{ session('employee')->employment_status }}</p>
+                    <p>Join Date: {{ session('employee')->join_date }}</p>
+                    <p>Monthly Salary: {{ session('employee')->salary }}</p>
+                    <p>Subsidy: {{ session('employee')->tunjangan }}</p>
+                </div>
             </div>
         </div>
     </div>
